9350f6c5214ddaf6130c13386502276756c2e568
«Maxim <realmazepa@gmail.com>
2022-12-03 17:03:54 +0300

    update003

---
+++ Example005Hello_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+Console.WriteLine("Привет, Маша! Как ты вообще? Норм?");    
+}
+else
+{
+    Console.Write("Проваливай в ебеня, ");
+    Console.WriteLine(username);
+}
