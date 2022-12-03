ecfe04c7c4b3d4fba6b42f26037b6c041bde3aea
«Maxim <realmazepa@gmail.com>
2022-12-03 16:20:21 +0300

    update 003

--- Example003Hello_sum/Program.cs
+++ Example003Hello_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberone = 2;
-int numbertwo = 4;
+﻿int numberone = new Random().Next(1,20); // 1,2,3,4...20
+Console.WriteLine(numberone);
+int numbertwo = new Random().Next(1,7);
+Console.WriteLine(numbertwo);
 int result = numberone + numbertwo;
 Console.WriteLine(result);
