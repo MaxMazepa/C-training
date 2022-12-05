b6e324e3068ef5c566ff72f2d14603ca927b2248
«Maxim <realmazepa@gmail.com>
2022-12-05 22:04:58 +0300

    Added Example 006

---
+++ Example006Hello_max/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 2;
+int b = 5;
+int c = 1;
+int d = 10;
+int e = 7;
+
+int max = a;
+
+if ( a>max ) max = a;
+if ( b>max ) max = b;
+if ( c>max ) max = c;
+if ( d>max ) max = d;
+if ( e>max ) max = e;
+
+Console.Write ("Максимальное значение: ");
+Console.WriteLine(max);
